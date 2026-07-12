7f233ecd7efbe6397aab553582a4f4d68b59ac02
JSv4 <scrudato@umich.edu>
2026-07-11 21:40:31 -0500

    chore: bump docxodus to 7.0.1 and refresh WASM
    Upgrade to Docxodus 7.0.1, which includes the DocumentSettingsPart
    null-check fix (upstream JSv4/Docxodus#264, reported via #27) so
    packages without word/settings.xml no longer crash ConvertToHtml
    with ArgumentNullException("part"), plus a companion
    StyleDefinitionsPart fix (JSv4/Docxodus#266).
    
    public/wasm was regenerated via the documented procedure
    (npm install + cp -r node_modules/docxodus/dist/wasm public/wasm),
    so it's byte-for-byte what npm publishes for this version.

---
+++ public/wasm/_framework/dotnet.boot.js
@@ -0,0 +1,279 @@
+export const config = /*json-start*/{
+  "mainAssemblyName": "DocxodusWasm.dll",
+  "resources": {
+    "hash": "sha256-zg2X0rOKlL4KMEPvPX/qp1JMyFKIA/EHaUg6Ch3fvAE=",
+    "jsModuleNative": [
+      {
+        "name": "dotnet.native.js"
+      }
+    ],
+    "jsModuleRuntime": [
+      {
+        "name": "dotnet.runtime.js"
+      }
+    ],
+    "wasmNative": [
+      {
+        "name": "dotnet.native.wasm",
+        "hash": "sha256-m8pVXIEyu68PkyPZ/GTr0HTn5lEPWxFrwVULaIsMgVs="
+      }
+    ],
+    "wasmSymbols": [
+      {
+        "name": "dotnet.native.js.symbols"
+      }
+    ],
+    "coreAssembly": [
+      {
+        "virtualPath": "System.Private.CoreLib.wasm",
+        "name": "System.Private.CoreLib.wasm",
+        "hash": "sha256-sBNbKBuqfDNazjDTIxARD1VelrWlgxyNYgAz7bQxocA="
+      },
+      {
+        "virtualPath": "System.Runtime.InteropServices.JavaScript.wasm",
+        "name": "System.Runtime.InteropServices.JavaScript.wasm",
+        "hash": "sha256-76enoQ0O6cR38Sts2lxQknOze11Dtss951aKHiNEST8="
+      }
+    ],
+    "assembly": [
+      {
+        "virtualPath": "DocumentFormat.OpenXml.wasm",
+        "name": "DocumentFormat.OpenXml.wasm",
+        "hash": "sha256-tA+aUbtQbHkyqB30uueND2hbKGToJ6iTp+Xf7pIH02k="
+      },
+      {
+        "virtualPath": "DocumentFormat.OpenXml.Framework.wasm",
+        "name": "DocumentFormat.OpenXml.Framework.wasm",
+        "hash": "sha256-c5bF1JZKGobvHZPUDwd+vncOjrdPRV0sJKlHZFBq6v8="
+      },
+      {
+        "virtualPath": "Docxodus.wasm",
+        "name": "Docxodus.wasm",
+        "hash": "sha256-AiLATRRk3d/pARdGNZOM3bO2NvShunXoaka8/FyR+5Y="
+      },
+      {
+        "virtualPath": "DocxodusWasm.wasm",
+        "name": "DocxodusWasm.wasm",
+        "hash": "sha256-jFjzuHGqLXJv8uYkWzFkUuJiYpt2TBEfxqk5BPieLN0="
+      },
+      {
+        "virtualPath": "System.Collections.Concurrent.wasm",
+        "name": "System.Collections.Concurrent.wasm",
+        "hash": "sha256-W0iBPMVxVS5yI1D21dsLIsNE+Q+5IZYGuyBwGmoR5dU="
+      },
+      {
+        "virtualPath": "System.Collections.wasm",
+        "name": "System.Collections.wasm",
+        "hash": "sha256-ipB4A+8DfPrfOLo1xOeQFABodC53frm3w9DiwnLUN9Q="
+      },
+      {
+        "virtualPath": "System.Collections.Immutable.wasm",
+        "name": "System.Collections.Immutable.wasm",
+        "hash": "sha256-+ZZD1IBl61OuVbLI6Fzq1fDkgWJZ69HH7xT/QkuSDzY="
+      },
+      {
+        "virtualPath": "System.Collections.NonGeneric.wasm",
+        "name": "System.Collections.NonGeneric.wasm",
+        "hash": "sha256-fyzoatDRkXAHhWq+CGDWZucGAhWh/QY/AP/gnFwxjBI="
+      },
+      {
+        "virtualPath": "System.Collections.Specialized.wasm",
+        "name": "System.Collections.Specialized.wasm",
+        "hash": "sha256-sBr4JHHVVPAm1PtPgu2mRzYbw9RIA4JcQLArxYWl8B0="
+      },
+      {
+        "virtualPath": "System.ComponentModel.wasm",
+        "name": "System.ComponentModel.wasm",
+        "hash": "sha256-FlqirVL9sIZuu5B2uysMetgDBYcJW58SWqLwNYQFKzY="
+      },
+      {
+        "virtualPath": "System.ComponentModel.Primitives.wasm",
+        "name": "System.ComponentModel.Primitives.wasm",
+        "hash": "sha256-T/mEK2+88809KxP7b+7VcMtyv5RPHbdVazWWWiNHSMk="
+      },
+      {
+        "virtualPath": "System.ComponentModel.TypeConverter.wasm",
+        "name": "System.ComponentModel.TypeConverter.wasm",
+        "hash": "sha256-gSIt1Bi6GN+6CWDXh1ESvRQ6OXPlPNh7fDpt7lS0JyE="
+      },
+      {
+        "virtualPath": "System.Console.wasm",
+        "name": "System.Console.wasm",
+        "hash": "sha256-MTBAU/l12UCuFMsS4qWPHY2SftxpS/VXaoim59ERrvE="
+      },
+      {
+        "virtualPath": "System.Diagnostics.Process.wasm",
+        "name": "System.Diagnostics.Process.wasm",
+        "hash": "sha256-mSk0o+Ccy/Flg0MKNNq6Y644OT0xkwVwCCxL0QfYgqs="
+      },
+      {
+        "virtualPath": "System.wasm",
+        "name": "System.wasm",
+        "hash": "sha256-TbXBiQa38E2TrjWSwZYk9l9sTztp8pQ4MLJyVPBpaEk="
+      },
+      {
+        "virtualPath": "System.IO.Compression.wasm",
+        "name": "System.IO.Compression.wasm",
+        "hash": "sha256-ueLM9k+hdkIl40D6cLA09MS8gG7wxOjqHlR1DCfoJLY="
+      },
+      {
+        "virtualPath": "System.IO.Packaging.wasm",
+        "name": "System.IO.Packaging.wasm",
+        "hash": "sha256-+maAKZe4KAjyWrRHK43LBexYakrtACptgDVGDrRW8fk="
+      },
+      {
+        "virtualPath": "System.IO.Pipelines.wasm",
+        "name": "System.IO.Pipelines.wasm",
+        "hash": "sha256-7N+scuN8nmiU+QbLyZpP/FeRS5FYzMOKMTSW7NAJetA="
+      },
+      {
+        "virtualPath": "System.Linq.wasm",
+        "name": "System.Linq.wasm",
+        "hash": "sha256-EemMFsvu7I2aBmKscJUP4MPIAbK2hMLwClp1optPyjA="
+      },
+      {
+        "virtualPath": "System.Linq.Expressions.wasm",
+        "name": "System.Linq.Expressions.wasm",
+        "hash": "sha256-iS8eS+5kyiSPpX+GvT3j1uLeYqjZHWD8+0KIOjggic0="
+      },
+      {
+        "virtualPath": "System.Memory.wasm",
+        "name": "System.Memory.wasm",
+        "hash": "sha256-HCAzIk0AMrOD2sdtanZaWRcXJ6NScSa7YzWCKSTjvy4="
+      },
+      {
+        "virtualPath": "System.Net.Http.wasm",
+        "name": "System.Net.Http.wasm",
+        "hash": "sha256-bl5ENLCV44VGRG7ea58q0Q+/2wRWvNw2wmxhTS5LVq8="
+      },
+      {
+        "virtualPath": "System.Net.Primitives.wasm",
+        "name": "System.Net.Primitives.wasm",
+        "hash": "sha256-wAP39lrPxd24Aksv911Fc9QeQhlMtivWi6RANnW+0+Q="
+      },
+      {
+        "virtualPath": "System.ObjectModel.wasm",
+        "name": "System.ObjectModel.wasm",
+        "hash": "sha256-+Ut3mfMMmlZKxBeaBlO6gzP16AnZSafEurMGorSdIWI="
+      },
+      {
+        "virtualPath": "System.Private.Uri.wasm",
+        "name": "System.Private.Uri.wasm",
+        "hash": "sha256-mL3RkFsg2COGs56nEZOuCf4qBmrFiFLSJR98unUNt/E="
+      },
+      {
+        "virtualPath": "System.Private.Xml.wasm",
+        "name": "System.Private.Xml.wasm",
+        "hash": "sha256-im4qsAZQbBb66Fpj5y/PJKHP56lNgli8gCg7U1RbbUw="
+      },
+      {
+        "virtualPath": "System.Private.Xml.Linq.wasm",
+        "name": "System.Private.Xml.Linq.wasm",
+        "hash": "sha256-Cfx1N4d9L14kN9LwDm0A/+qUsnVOZuzGv/xgurmx3aI="
+      },
+      {
+        "virtualPath": "System.Runtime.wasm",
+        "name": "System.Runtime.wasm",
+        "hash": "sha256-R2j0vuiSt9T7tZV9JxndwsO3EQzz0lNk6xkTxJfqYZc="
+      },
+      {
+        "virtualPath": "System.Security.Cryptography.wasm",
+        "name": "System.Security.Cryptography.wasm",
+        "hash": "sha256-axoOi6eREjVg1KhxZrOGKIJ5sFj0ATHekFUQHg7GdHw="
+      },
+      {
+        "virtualPath": "System.Text.Encoding.Extensions.wasm",
+        "name": "System.Text.Encoding.Extensions.wasm",
+        "hash": "sha256-/TR7qzVVJn7DmAnqx7TDcP6BAHH4oc07Id9fm3oC1Y0="
+      },
+      {
+        "virtualPath": "System.Text.Encodings.Web.wasm",
+        "name": "System.Text.Encodings.Web.wasm",
+        "hash": "sha256-sc8idhMkUTgr7lFFlmuj+QEpDtP0HQqQXKOHeoHhMIg="
+      },
+      {
+        "virtualPath": "System.Text.Json.wasm",
+        "name": "System.Text.Json.wasm",
+        "hash": "sha256-wEg7j9grLWVF82JtlFwOVGMaukWuWAXKGRBYpdIjDII="
+      },
+      {
+        "virtualPath": "System.Text.RegularExpressions.wasm",
+        "name": "System.Text.RegularExpressions.wasm",
+        "hash": "sha256-90sQeqQjb9e3T+2ceeaIiR9Hmjs+Blx/I18w0PlBYYI="
+      },
+      {
+        "virtualPath": "System.Threading.wasm",
+        "name": "System.Threading.wasm",
+        "hash": "sha256-hNrDFtOWEF1g2Rgqlz+zM90S1gHj+8ooh1lbC8Ya3wY="
+      },
+      {
+        "virtualPath": "System.Threading.Thread.wasm",
+        "name": "System.Threading.Thread.wasm",
+        "hash": "sha256-kKrWJXUBgwVIgKT59n8FCjf3gSm6OmZwaGpH/6QEDnM="
+      },
+      {
+        "virtualPath": "System.Xml.Linq.wasm",
+        "name": "System.Xml.Linq.wasm",
+        "hash": "sha256-T+9RdfUBfnlvro21wk8VmmYe3ZDfKiYEBbJPCfLiuds="
+      },
+      {
+        "virtualPath": "System.Xml.ReaderWriter.wasm",
+        "name": "System.Xml.ReaderWriter.wasm",
+        "hash": "sha256-1rhifEPRgirdZkeUo1VlEgDmJeBgn7iN1bfKwQ883Pg="
+      },
+      {
+        "virtualPath": "System.Xml.XDocument.wasm",
+        "name": "System.Xml.XDocument.wasm",
+        "hash": "sha256-LofpKn/53U2PH/jKFLi7HPQHB70/2rvdBGF2xiEIZwI="
+      },
+      {
+        "virtualPath": "System.Xml.XPath.wasm",
+        "name": "System.Xml.XPath.wasm",
+        "hash": "sha256-ovLFduyaX9YgP/Ifl4S5t3ivTsguUScKyZ9+fMRX34U="
+      },
+      {
+        "virtualPath": "System.Xml.XPath.XDocument.wasm",
+        "name": "System.Xml.XPath.XDocument.wasm",
+        "hash": "sha256-EVlliH6tXJmh6nplIjVZaH/mLV8WrnxBnfRkVX+2y8Q="
+      }
+    ]
+  },
+  "debugLevel": 0,
+  "globalizationMode": "invariant",
+  "runtimeConfig": {
+    "runtimeOptions": {
+      "configProperties": {
+        "Microsoft.Extensions.DependencyInjection.VerifyOpenGenericServiceTrimmability": true,
+        "System.ComponentModel.DefaultValueAttribute.IsSupported": false,
+        "System.ComponentModel.Design.IDesignerHost.IsSupported": false,
+        "System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization": false,
+        "System.ComponentModel.TypeDescriptor.IsComObjectDescriptorSupported": false,
+        "System.Data.DataSet.XmlSerializationIsSupported": false,
+        "System.Diagnostics.Debugger.IsSupported": false,
+        "System.Diagnostics.Metrics.Meter.IsSupported": false,
+        "System.Diagnostics.Tracing.EventSource.IsSupported": false,
+        "System.Globalization.Invariant": true,
+        "System.TimeZoneInfo.Invariant": false,
+        "System.Globalization.PredefinedCulturesOnly": true,
+        "System.Linq.Enumerable.IsSizeOptimized": true,
+        "System.Net.Http.EnableActivityPropagation": false,
+        "System.Net.Http.UseNativeHttpHandler": true,
+        "System.Net.Http.WasmEnableStreamingResponse": true,
+        "System.Net.SocketsHttpHandler.Http3Support": false,
+        "System.Reflection.Metadata.MetadataUpdater.IsSupported": false,
+        "System.Resources.ResourceManager.AllowCustomResourceTypes": false,
+        "System.Resources.UseSystemResourceKeys": true,
+        "System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported": true,
+        "System.Runtime.InteropServices.BuiltInComInterop.IsSupported": false,
+        "System.Runtime.InteropServices.EnableConsumingManagedCodeFromNativeHosting": false,
+        "System.Runtime.InteropServices.EnableCppCLIHostActivation": false,
+        "System.Runtime.InteropServices.Marshalling.EnableGeneratedComInterfaceComImportInterop": false,
+        "System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization": false,
+        "System.StartupHookProvider.IsSupported": false,
+        "System.Text.Encoding.EnableUnsafeUTF7Encoding": false,
+        "System.Text.Json.JsonSerializer.IsReflectionEnabledByDefault": false,
+        "System.Threading.Thread.EnableAutoreleasePool": false
+      }
+    }
+  }
+}/*json-end*/;
